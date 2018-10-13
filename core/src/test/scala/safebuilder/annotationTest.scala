2c734eaf6eae18f29ee6aaaa88f189fb98a457eb
Paweł Cejrowski <pcejrowski@gmail.com>
2018-10-13 20:28:41 +0200

    Refactor tests
    FlatSpec -> FreeSpec
    splitting tests

--- core/src/test/scala/safebuilder/annotationTest.scala
+++ core/src/test/scala/safebuilder/annotationTest.scala
@@ -1,82 +1,102 @@
 package safebuilder
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FreeSpec, Matchers}
 import safebuilder.annotation.builder
 
-class annotationTest extends FlatSpec with Matchers {
+class annotationTest extends FreeSpec with Matchers {
 
-  behavior of "@builder"
-  it should "build if all params are there" in {
-    @builder case class Foo(foo: Option[String], bar: Boolean, baz: Either[Float, String])
-    val testee = Foo()
-      .withFoo("foo-string")
-      .barEnabled
-      .withBaz(.3f)
-      .build
-    testee.foo should be(Some("foo-string"))
-    testee.bar should be(true)
-    testee.baz should be(Left(.3f))
+  "@builder" - {
+    "doesn't affect existing functionality" - {
+      "built-in apply works" in {
+        @builder case class Foo(foo: String)
+        Foo("foo").foo shouldBe "foo"
+      }
+    }
+
+    "has support for type:" - {
+      "Option" in {
+        @builder case class Foo(foo: Option[String], bar: Option[Int], baz: Option[Boolean])
+        val testee = Foo()
+          .withFoo("foo-string")
+          .withBaz(false)
+          .build
+        testee.foo shouldBe Some("foo-string")
+        testee.bar shouldBe None
+        testee.baz shouldBe Some(false)
+      }
+      "Either" in {
+        @builder case class Foo(foo: Either[Float, String], bar: Either[Float, String])
+        val testee = Foo()
+          .withFoo(.3f)
+          .withBar("bar-string")
+          .build
+        testee.foo shouldBe Left(.3f)
+        testee.bar shouldBe Right("bar-string")
+      }
+      "Boolean" in {
+        @builder case class Foo(foo: Boolean, bar: Boolean)
+        val testee = Foo()
+          .fooEnabled
+          .barDisabled
+          .build
+        testee.foo shouldBe true
+        testee.bar shouldBe false
+      }
+      "Any (...other)" in {
+        case class Baz(baz: String)
+        @builder case class Foo(foo: String, bar: Float, baz: Baz)
+        val testee = Foo()
+          .withFoo("foo")
+          .withBar(.3f)
+          .withBaz(Baz("baz"))
+          .build
+        testee.foo shouldBe "foo"
+        testee.bar shouldBe .3f
+        testee.baz shouldBe Baz("baz")
+      }
+    }
+    "respects default values" - {
+      "doesn't require to specify it" in {
+        @builder case class Foo(foo: String = "foo")
+        val testee = Foo().build
+        testee.foo shouldBe "foo"
+      }
+      "allows to overwrite it" in {
+        @builder case class Foo(foo: String = "foo")
+        val testee = Foo()
+          .withFoo("diff-foo")
+          .build
+        testee.foo shouldBe "diff-foo"
+      }
+    }
+    "allow annotating more case classes" - {
+      @builder case class Foo(foo: Option[String])
+      @builder case class Bar(bar: Boolean)
+
+      val testee1 = Foo().withFoo("foo-string").build
+      testee1.foo shouldBe Some("foo-string")
+
+      val testee2 = Bar().barEnabled.build
+      testee2.bar shouldBe true
+    }
+    "fails if applied badly" - {
+      "on not a case class" in {
+        assertTypeError("""@builder object Foo""")
+        assertTypeError("""@builder class Foo""")
+        assertTypeError("""@builder trait Foo""")
+      }
+      "on a case class without parameters" in {
+        assertTypeError("""@builder case class Foo()""")
+      }
+    }
+    "not build if not all parameters are defined" - {
+      @builder case class Foo(foo: Option[String], bar: Boolean, baz: Either[Float, String])
+      assertTypeError("""Foo().withFoo("foo").build""")
+      assertTypeError("""Foo().withBar(7).build""")
+      assertTypeError("""Foo().withBaz(.3f).build""")
+      assertTypeError("""Foo().withFoo("foo").withBar(7).build""")
+      assertTypeError("""Foo().withFoo("foo").withBaz(.3f).build""")
+      assertTypeError("""Foo().withBar(7).withBaz(.3f).build""")
+    }
   }
-
-  it should "build if all non-optional params are there" in {
-    @builder case class Foo(foo: Option[String], bar: Boolean, baz: Either[Float, String])
-    val testee = Foo()
-      .barEnabled
-      .withBaz(.3f)
-      .build
-    testee.foo should be(None)
-    testee.bar should be(true)
-    testee.baz should be(Left(.3f))
-  }
-
-  it should "respect default value" in {
-    @builder case class Foo(foo: String = "foo")
-    val testee = Foo().build
-    testee.foo should be("foo")
-  }
-
-  it should "allow to overwrite default value" in {
-    @builder case class Foo(foo: String = "foo")
-    val testee = Foo()
-      .withFoo("diff-foo")
-      .build
-    testee.foo should be("diff-foo")
-  }
-
-  it should "not break the built-in apply method" in {
-    @builder case class Foo(foo: String)
-    Foo("foo").foo should be("foo")
-  }
-
-  it should "not build if not all params are there" in {
-    @builder case class Foo(foo: Option[String], bar: Boolean, baz: Either[Float, String])
-    assertTypeError("""Foo().withFoo("foo").build""")
-    assertTypeError("""Foo().withBar(7).build""")
-    assertTypeError("""Foo().withBaz(.3f).build""")
-    assertTypeError("""Foo().withFoo("foo").withBar(7).build""")
-    assertTypeError("""Foo().withFoo("foo").withBaz(.3f).build""")
-    assertTypeError("""Foo().withBar(7).withBaz(.3f).build""")
-  }
-
-  it should "allow annotating more case classes" in {
-    @builder case class Foo(foo: Option[String])
-    @builder case class Bar(bar: Boolean)
-
-    val testee1 = Foo().withFoo("foo-string").build
-    testee1.foo should be(Some("foo-string"))
-
-    val testee2 = Bar().barEnabled.build
-    testee2.bar should be(true)
-  }
-
-  it should "fail on a case class without parameters" in {
-    assertTypeError("""@builder case class Foo()""")
-  }
-
-  it should "fail on not a case class" in {
-    assertTypeError("""@builder object Foo""")
-    assertTypeError("""@builder class Foo""")
-    assertTypeError("""@builder trait Foo""")
-  }
-
 }
--- core/src/test/scala/safebuilder/phantomsTest.scala
+++ core/src/test/scala/safebuilder/phantomsTest.scala
@@ -1,40 +1,41 @@
 package safebuilder
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FreeSpec, Matchers}
 import safebuilder.phantoms._
 
-class phantomsTest extends FlatSpec with Matchers {
+class phantomsTest extends FreeSpec with Matchers {
 
-  behavior of "phantom types"
+  "phantom types" - {
 
-  they should "match to each other" in {
-    implicitly[TTrue =:= TTrue]
-    implicitly[TFalse =:= TFalse]
-  }
-  they should "flip the value" in {
-    implicitly[Not[TTrue] =:= TFalse]
-    implicitly[Not[TFalse] =:= TTrue]
-  }
-  they should "sum properly" in {
-    implicitly[TFalse Or TFalse =:= TFalse]
-    implicitly[TFalse Or TTrue =:= TTrue]
-    implicitly[TTrue Or TFalse =:= TTrue]
-    implicitly[TTrue Or TTrue =:= TTrue]
-  }
-  they should "intersect properly" in {
-    implicitly[TFalse And TFalse =:= TFalse]
-    implicitly[TFalse And TTrue =:= TFalse]
-    implicitly[TTrue And TFalse =:= TFalse]
-    implicitly[TTrue And TTrue =:= TTrue]
-  }
-  they should "apply mixed operations properly" in {
-    implicitly[Not[TFalse And TTrue] =:= TTrue]
-    implicitly[TTrue And Not[TTrue] =:= TFalse]
-  }
-  they should "not accept any bullshit" in {
-    assertTypeError("implicitly[TTrue =:= TFalse]")
-    assertTypeError("implicitly[Not[TTrue] =:= TTrue]")
-    assertTypeError("implicitly[TFalse Or TTrue =:= TFalse]")
-    assertTypeError("implicitly[TTrue And TFalse =:= TTrue]")
+    "match to each other" - {
+      implicitly[TTrue =:= TTrue]
+      implicitly[TFalse =:= TFalse]
+    }
+    "flip the value" - {
+      implicitly[Not[TTrue] =:= TFalse]
+      implicitly[Not[TFalse] =:= TTrue]
+    }
+    "sum properly" - {
+      implicitly[TFalse Or TFalse =:= TFalse]
+      implicitly[TFalse Or TTrue =:= TTrue]
+      implicitly[TTrue Or TFalse =:= TTrue]
+      implicitly[TTrue Or TTrue =:= TTrue]
+    }
+    "intersect properly" - {
+      implicitly[TFalse And TFalse =:= TFalse]
+      implicitly[TFalse And TTrue =:= TFalse]
+      implicitly[TTrue And TFalse =:= TFalse]
+      implicitly[TTrue And TTrue =:= TTrue]
+    }
+    "apply mixed operations properly" - {
+      implicitly[Not[TFalse And TTrue] =:= TTrue]
+      implicitly[TTrue And Not[TTrue] =:= TFalse]
+    }
+    "respect the boolean logic" - {
+      assertTypeError("implicitly[TTrue =:= TFalse]")
+      assertTypeError("implicitly[Not[TTrue] =:= TTrue]")
+      assertTypeError("implicitly[TFalse Or TTrue =:= TFalse]")
+      assertTypeError("implicitly[TTrue And TFalse =:= TTrue]")
+    }
   }
 }
